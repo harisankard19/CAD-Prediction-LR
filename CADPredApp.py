6af5be22c35fbf65a92c726a6425ae8fd5648094
Harisankar D <128053337+harisankard19@users.noreply.github.com>
2025-03-30 14:21:55 +0530

    Update CADPredApp.py

--- CADPredApp.py
+++ CADPredApp.py
@@ -3,7 +3,8 @@ import pickle
 import streamlit as st
 
 # Load trained model
-loaded_model = pickle.load(open('C:/Users/Admin/OneDrive/Desktop/acadX/1 AIML and Data Science/Machine Learning Siddharth Course/CAD App Deployment Streamlit/trained_model.sav', 'rb'))
+loaded_model = pickle.load(open('trained_model.sav', 'rb'))
+
 
 # Function for prediction
 def CAD_prediction(input_data):
